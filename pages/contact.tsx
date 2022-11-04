6af137f9d04bc7bd89f5c281779b7b48aab95c32
Cleaver Barnes <cbarnes@cleaver.ca>
2022-11-04 19:13:24 -0400

    add contact page

---
+++ pages/contact.tsx
@@ -0,0 +1,19 @@
+function ContactPage() {
+  return (
+    <article>
+      <h1>Contact</h1>
+      <div className="font-mono leading-5">
+        &nbsp;/\_/\
+        <br />
+        ( o.o )<br />
+        &nbsp;&gt; ^ &lt;
+      </div>
+
+      <p>
+        Got inquiries or comments? Say hi at hellosorrowbacon [at] gmail.com.
+      </p>
+    </article>
+  );
+}
+
+export default ContactPage;
