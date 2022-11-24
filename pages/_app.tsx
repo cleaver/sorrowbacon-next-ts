e47e460e0ed6dba438c833c1a9a9de440bb82af3
Cleaver Barnes <cbarnes@cleaver.ca>
2022-11-24 12:11:07 -0500

    add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <Layout>
       <Head>
         <meta name="og:type" content="website" />
+        <link rel="shortcut icon" href="/sb-favicon.png" type="image/png" />
       </Head>
       <main className="prose max-w-none">
         <Component {...pageProps} />
