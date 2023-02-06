9d0a34d27fcbf7cd59d0da06357e2bf0c19e22c6
Cleaver Barnes <git@cleaver.ca>
2023-02-05 21:55:04 -0500

    allow images remote from prod

--- next.config.js
+++ next.config.js
@@ -14,6 +14,10 @@ const nextConfig = {
         protocol: 'http',
         hostname: 'strapi',
       },
+      {
+        protocol: 'https',
+        hostname: 'sb.pimpsmooth.com',
+      },
     ],
   },
   async redirects() {
