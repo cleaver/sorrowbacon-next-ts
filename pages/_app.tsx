c2e69ecf0fa57cc7bd1fafac511dd81ecc8ab791
Cleaver Barnes <295771+cleaver@users.noreply.github.com>
2023-08-03 17:29:38 -0400

    update ga4 code to use next/script - see: https://dev.to/asross311/strongly-typed-ga4-with-nextjs-part-ii-danger-zone-26f

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,11 @@
-import type { AppProps } from 'next/app';
+import type { AppProps, NextWebVitalsMetric } from 'next/app';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
+import Script from 'next/script';
 import { useEffect } from 'react';
 import Layout from '../components/layout/layout';
 import * as gtag from '../lib/analytics';
+import { GA_TRACKING_ID } from '../lib/config';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -21,6 +23,21 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <Layout>
+      <Script
+        async
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+      />
+      <Script strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_TRACKING_ID}', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
       <Head>
         <meta name="og:type" content="website" />
         <link rel="shortcut icon" href="/sb-favicon.png" type="image/png" />
@@ -33,3 +50,19 @@ function MyApp({ Component, pageProps }: AppProps) {
 }
 
 export default MyApp;
+
+export function reportGAVitals({
+  id,
+  name,
+  label,
+  value,
+}: NextWebVitalsMetric) {
+  if (typeof window !== 'undefined')
+    window.gtag('event', name, {
+      event_category:
+        label === 'web-vital' ? 'Web Vitals' : 'Next.js custom metric',
+      value: Math.round(name === 'CLS' ? value * 1000 : value),
+      event_label: id,
+      non_interaction: true,
+    } as Gtag.EventParams);
+}
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,24 +4,7 @@ import { GA_TRACKING_ID } from '../lib/config';
 const Document = () => {
   return (
     <Html lang="en-CA">
-      <Head>
-        <meta charSet="utf-8" />
-        <script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-
-                    gtag('config', '${GA_TRACKING_ID}', {
-                        page: window.location.pathname
-                    });`,
-          }}
-        />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
